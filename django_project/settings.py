02176f2b4885c2e282917a78a5cced3bd90426bb
nkossnar <nkossnar@gmail.com>
2023-04-09 17:19:19 -0700

    ugh

--- django_project/settings.py
+++ django_project/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG")
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["glacial-chamber-64384.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
