74641b1033b5c00ca262cd1d723f65ac706aa98f
nkossnar <nkossnar@gmail.com>
2023-04-15 11:57:54 -0700

    path name and added sentry documentation code to settings and url

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,11 @@
 from django.urls import path
 from .views import home_page_view, home, react
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path("", home, name="home"),
     path("react/", react, name="react"),
+    path('sentry-debug/', trigger_error),
 ]
