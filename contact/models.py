10b19adc6b9564230af48f6c121013707ef71881
nkossnar <nkossnar@gmail.com>
2023-04-16 18:28:12 -0700

    removed data from contact model

--- contact/models.py
+++ contact/models.py
@@ -6,7 +6,6 @@ class Contact(models.Model):
     email = models.EmailField()
     subject = models.CharField(max_length=100)
     message = models.TextField()
-    submitted = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return str(self.id)
